0de3357643766ac89e0a0c10a70bd461725bf6a6
Shankar Swamy <shankar.swamy@outlook.com>
2024-08-14 15:53:47 -0700

    [csc133Driver.java]: Initial commit

---
+++ src/csc133/csc133Driver.java
@@ -0,0 +1,17 @@
+package csc133;
+
+import mechanicsBE.slTTTBoard;
+
+public class csc133Driver {
+    public static void main(String[] args) {
+        slTTTBoard my_board = new slTTTBoard();
+
+        my_board.printBoard();
+    }
+
+
+
+
+}  // public class csc133Driver
+
+
