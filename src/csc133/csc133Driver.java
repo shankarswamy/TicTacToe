492466dd3251e2eb9c1023429c4c07fe9b36df12
Shankar Swamy <shankar.swamy@outlook.com>
2024-09-09 06:48:11 -0700

    [csc133.java, gameFE.java]: Data-Oriented Design of TicTacToe complete.
    Make Main class perform data conduit action completely.

--- src/csc133/csc133Driver.java
+++ src/csc133/csc133Driver.java
@@ -1,15 +1,35 @@
 package csc133;
 
 import mechanicsBE.slTTTBoard;
+import static csc133.gameFE.*;
+
+import static csc133.spot.GAME_INCOMPLETE;
+import static csc133.spot.GAME_QUIT;
 
 
 public class csc133Driver {
+    private final slTTTBoard my_board = new slTTTBoard();
+    private final csc133.gameFE my_fe = new csc133.gameFE();
     public static void main(String[] args) {
-
-        slTTTBoard my_board = new slTTTBoard();
-        (new csc133.gameFE(my_board)).startGame();
+        (new csc133Driver()).startGame();
 
     }  //  public static void main(String] args)
+
+    private void startGame() {
+        my_board.printBoard();
+        int game_status = GAME_INCOMPLETE;
+        while (GAME_QUIT != game_status) {
+            my_fe.print_exit_message(game_status);
+            my_board.resetBoard();
+            if (my_fe.promptToStart()) {
+                my_board.playRandom();
+                my_board.printBoard();
+            }  // if (my_fe.promptToStart())
+            game_status = my_board.play();
+        }  //  while (...)
+        my_fe.promptToStart();
+        // Game has ended; display a message here based on game_status
+    }  // public static void startGame()
 
 }  // public class csc133Driver
 
--- src/csc133/gameFE.java
+++ src/csc133/gameFE.java
@@ -10,31 +10,8 @@ import static csc133.spot.*;
 import static java.lang.System.exit;
 
 public class gameFE {
-
-    slTTTBoard my_board;
-
-    gameFE(slTTTBoard game_board) {
-        my_board = game_board;
-    }
-
-    public void startGame() {
-        my_board.printBoard();
-        int game_status = GAME_INCOMPLETE;
-        while (GAME_QUIT != game_status) {
-            print_exit_message(game_status);
-            my_board.resetBoard();
-            if (promptToStart()) {
-                my_board.playRandom();
-                my_board.printBoard();
-            }
-            game_status = my_board.play();
-        }  //  while (...)
-        print_exit_message(game_status);
-        // Game has ended; display a message here based on game_status
-    }  // public static void startGame()
-
     //  return true --> machine; false --> player
-    private static boolean promptToStart() {
+    public boolean promptToStart() {
         boolean retVal = false;
         System.out.println("\nWould you like to start? 'n' for machine to start");
         System.out.print("'y' if you want to start the game:\n");
@@ -52,7 +29,7 @@ public class gameFE {
         return retVal;
     }  //  boolean promptToStart()
 
-    private static void print_exit_message ( int exit_status){
+    public void print_exit_message(int exit_status){
         switch (exit_status) {
             case GAME_QUIT:
                 System.out.println("Sorry to see you go; come again!");
